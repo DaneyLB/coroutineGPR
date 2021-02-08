f55f467a0744406dd86f94e0f07da164f536221f
DaneyLB <daney@bb-admin.nl>
2021-02-08 12:39:44 +0100

    Coroutine opdracht 1

---
+++ Assets/Scripts/CodeRoutine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeRoutine : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        StartCoroutine(Print());
+
+    }
+    IEnumerator Print()
+    {
+        Debug.Log("Ik start nu de coroutine");
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("coroutine update");
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("coroutine einde");
+    }
+}
